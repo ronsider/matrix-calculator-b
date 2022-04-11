7369be9a91c9b469649eddeb04f8dbae5f018109
ronsider <ronsider2@gmail.com>
2022-04-11 11:34:10 +0300

    add functions

--- sources/Matrix.cpp
+++ sources/Matrix.cpp
@@ -2,7 +2,6 @@
 namespace zich
 {
 
-
 Matrix& Matrix::operator-()
 {
     for(auto i:vm)
@@ -13,22 +12,53 @@ Matrix& Matrix::operator-()
 }
 std::ostream &operator<<(std::ostream &os, const Matrix &m) 
 {
+    for(int i=0;i<m.m_rows;i++)
+    {
+        os<<'[';
+        for(int j=0;j<m.m_columns;j++)
+        {
+           os<<' '<<m.vm.at(j); 
+        }
+        os<<"]\n";
+    }
     return os;
 }
 Matrix& Matrix::operator* (double num1)
 {
-        return *this;
+    for(auto i:vm)
+    {
+        i*=num1;
+    }
+    return *this;
 }
 Matrix operator*(double number, const Matrix &mat)
 {
-    return mat;
+    vector<double>t;
+    t.resize(mat.m_rows*mat.m_columns);
+    for(int i=0;i<mat.vm.size();i++)
+    {
+        t.at(i)=mat.vm.at(i)*number;
+    }
+    Matrix m{t,mat.m_rows,mat.m_columns};
+    return m;
 }
 Matrix& Matrix::operator*= (double a)
 {
+    for(auto i:this->vm)
+    {
+        i*=a;
+    }
     return *this;
 }
 Matrix& Matrix::operator+ (const Matrix& mat)
 {
+    if(m_columns!=mat.m_columns || m_rows!=mat.m_rows)
+    throw "can't add 2 matrices, not the same dimensions";
+
+    for(int i=0;i<mat.vm.size();i++)
+    {
+        vm.at(i)+=mat.vm.at(i);
+    }
     return *this;
 }
 Matrix& Matrix::operator- (const Matrix& mat)
@@ -44,3 +74,5 @@ bool Matrix::operator!= (const Matrix &mat) const
         return false;
 }
 }
+
+
