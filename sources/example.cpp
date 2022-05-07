dc73dfb70f16858c926a43f9eeb304cad3c110e7
ronsider <ronsider2@gmail.com>
2022-05-07 21:10:49 +0300

    show prog. functionlity

---
+++ sources/example.cpp
@@ -0,0 +1,37 @@
+// //this program meant for demnstratig to TA the code functonlity
+
+// #include "Matrix.hpp"
+// using namespace zich;
+// using std::cout;
+
+// //command to demonstrate program functionality:
+// //clang++-9 Matrix.cpp example.cpp -o te
+// int main()
+// {
+    
+
+//     vector<double>v1{1,1,1,1,1,1,1,1,1};
+//     Matrix m1{v1,3,3};//a 3x3 of ones
+//     cout<<m1<<"\n\n\n";
+
+//     vector<double>v2{2,2,2,2,2,2,2,2,2};
+//     Matrix m2{v2,3,3};//a 3x3 of twos
+//     cout<<m2<<"\n\n\n";
+
+
+//     Matrix result{m1+m2};
+//     cout<<result<<"\n\n\n";
+
+//     cout<<result++<<"\n\n\n";
+
+//     cout<<++result<<"\n\n\n";
+
+//     result*=2;
+//     cout<<result<<"\n\n\n\n";
+
+//     vector<double>mul_ch{2,2,2,2,2,2,2,2,2};
+//     Matrix mul_check{mul_ch,3,3};
+
+//     cout<<result*mul_check<<"\n\n\n";
+//     return 0;
+// }
